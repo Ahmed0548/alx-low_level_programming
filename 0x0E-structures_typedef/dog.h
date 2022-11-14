34a901f517bd9691d2ff31950759e80128529175
Ahmed0548 <ahmedalkasape@gmail.com>
2022-11-14 08:54:54 -0800

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,14 @@
-#include "dog.h"
-#include <stdlib.h>
-void init_dog(struct dog *d, char *name, float age, char *owner)
+#ifndef DOG_H
+#define DOG_H
+struct dog
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
