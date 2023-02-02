ffc7c38821efcceb3ce9385161d4d6b12b97923a
Ahmed0548 <ahmedalkasape@gmail.com>
2023-02-02 09:02:43 -0800

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i = 0;
-
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
 	if (ht == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
@@ -26,4 +26,3 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	return (ht);
 }
-
